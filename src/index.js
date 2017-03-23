a88d536016c32fa11c1822b186fad8896e808644
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2017-03-23 09:36:27 +0100

    implement emiter receiver

--- src/index.js
+++ src/index.js
@@ -1,25 +1,108 @@
 import React, {Component} from 'react'
 
-export class Emitter extends Component {
-  constructor(props) {
-    super(props)
+export function createEmitterReceiver() {
+  let emitterIdentifier = 0, receiverIdentifier = 0
+  const emittersChildren = {}, receivers = {}
+
+  function sanitizeChildren(children) {
+    if (children === undefined || children === null) {
+      return []
+    }
+    if (Array.isArray(children)) {
+      return children
+    }
+    return [children]
   }
 
-  render() {
-    return null
+  function getReceiverChildren() {
+    return Object.keys(emittersChildren).reduce((children, id) => [
+      ...children,
+      ...emittersChildren[id],
+    ], [])
   }
+
+  function registerReceiver(receiver) {
+    const id = receiverIdentifier++
+    receivers[id] = receiver
+    return () => {
+      delete receivers[id]
+    }
+  }
+
+  function registerEmitter(children) {
+    const id = emitterIdentifier++
+    emittersChildren[id] = sanitizeChildren(children)
+    function emit() {
+      const receiverChildren = getReceiverChildren()
+      Object.keys(receivers).forEach((id) => receivers[id].update(receiverChildren))
+    }
+    return {
+      emit(children) {
+        emittersChildren[id] = sanitizeChildren(children)
+        emit()
+      },
+      unmount() {
+        delete emittersChildren[id]
+        emit()
+      },
+    }
+  }
+
+  class Emitter extends Component {
+    constructor(props) {
+      super(props)
+      const {emit, unmount} = registerEmitter(props.children)
+      this.emit = emit
+      this.unmount = unmount
+    }
+
+    componentWillUnmount() {
+      this.unmount()
+    }
+
+    componentDidMount() {
+      this.emit(this.props.children)
+    }
+
+    componentDidUpdate() {
+      this.emit(this.props.children)
+    }
+
+    render() {
+      return null
+    }
+  }
+
+  class Receiver extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        children: getReceiverChildren(),
+      }
+      this.update = this.update.bind(this)
+      this.unmount = registerReceiver(this)
+    }
+
+    update(children) {
+      this.setState({children})
+    }
+
+    componentWillUnmount() {
+      this.unmount()
+    }
+
+    render() {
+      return (
+        <div>
+          {this.state.children}
+        </div>
+      )
+    }
+  }
+
+  return {Emitter, Receiver}
 }
 
-export class Receiver extends Component {
-  constructor(props) {
-    super(props)
-  }
+const {Emitter, Receiver} = createEmitterReceiver();
 
-  render() {
-    return (
-      <div>
-        {null}
-      </div>
-    )
-  }
-}
+export {Emitter, Receiver}
